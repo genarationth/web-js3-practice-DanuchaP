7552f877428b23a720be5c40a4519f1718edcf98
dcpn <danucha.phol@gmail.com>
2023-03-13 16:51:19 +0700

    Create functions

---
+++ function2.js
@@ -0,0 +1,5 @@
+function introduceMyself(firstName, location, hobby) {
+    console.log("Hi, my name is " + firstName + ". I live in " + location + " and enjoy " + hobby + ".");
+  }
+
+  introduceMyself("Danucha", "Bangkok", "swimming");
